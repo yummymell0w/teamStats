f174ac27b5e7ae08ea97cb586b6802ad134f6ec7
Margo Art <margoart.work@gmail.com>
2017-10-28 20:59:27 -0500

    completed exercise Team Stats

--- app.js
+++ app.js
@@ -19,9 +19,21 @@ let team = {
            age: age
         };
         this._players.push(player);
+    },
+    addGame(opponent,teamPoints,opponentPoints) {
+        let game = {
+            opponent: opponent,
+            teamPoints: teamPoints,
+            opponentPoints: opponentPoints
+        };
+        this._games.push(game);
     }
 };
 team.addPlayer('Steph','Curry',28);
 team.addPlayer('Lisa','Leslie',42);
 team.addPlayer('Bugs','Bunny',76);
 console.log(team.players);
+team.addGame('Cubs',44,66);
+team.addGame('Victory',44,66);
+team.addGame('JS',44,66);
+console.log(team.games);
